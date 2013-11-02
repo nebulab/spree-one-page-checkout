fa2abd4149cc39cfe06031a62b94f1b8f337b0e2
Alberto Vena <kennyadsl@gmail.com>
2013-11-02 01:36:42 +0000

    initial commit (clean spree 2.1)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '19a5079772212634c2dac642b339214cfc103cccf1b7316a1dbba2f7129fdd075fa79d458fd7b2939ef484ff58347eb3f65cfcf4f20f992152e85f4392ccce9c'
---
+++ db/migrate/20131102013458_update_adjustment_states.spree.rb
@@ -0,0 +1,17 @@
+# This migration comes from spree (originally 20130417120035)
+class UpdateAdjustmentStates < ActiveRecord::Migration
+  def up
+    Spree::Order.complete.each do |order|
+      order.adjustments.update_all(:state => 'closed')
+    end
+
+    Spree::Shipment.shipped.each do |shipment|
+      shipment.adjustment.update_column(:state, 'finalized') if shipment.adjustment
+    end
+
+    Spree::Adjustment.where(:state => nil).update_all(:state => 'open')
+  end
+
+  def down
+  end
+end
